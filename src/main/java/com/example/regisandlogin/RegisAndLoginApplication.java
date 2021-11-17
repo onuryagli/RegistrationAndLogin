6611fc603baa261d1a86beeb8febfde5381c0cee
unknown <onur_yagli@hotmail.com>
2021-11-17 12:00:02 +0300

    First Commit

---
+++ src/main/java/com/example/regisandlogin/RegisAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.regisandlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisAndLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/regisandlogin/RegisAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regisandlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
